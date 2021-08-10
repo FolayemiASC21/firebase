70683c8cd901fb8cd3cbf8b144aae978ccd2f844
FolayemiASC21 <fkehinde@brooklynfriends.org>
2021-08-10 16:11:59 -0400

    first commit

--- script.js
+++ script.js
@@ -3,6 +3,8 @@ let messageElement = document.querySelector("#message");
 let button = document.querySelector("#submitButton");
 // Set database object REFERENCE here:
 
+let database = firebase.database().ref();
+
 /**
  * Updates the database with the username and message.
  */
@@ -10,9 +12,26 @@ button.onclick = function updateDB(event){
     event.preventDefault(); //stop refreshing
     let username        = usernameElement.value;
     let message         = messageElement.value;
-    usernameElement.value = "";
     messageElement.value  = "";
     console.log(username + " : " + message);
     // Update database here
+    let value = {
+        Name: username,
+        Message: message
+    }
 
+    database.push(value)
+}
+
+// Set database "child_added" event
+database.on("child_added",addMessageToBoard);
+
+function addMessageToBoard(rowData){
+let board = document.querySelector(".allMessages")
+let row = rowData.val()
+let message = document.createElement("p")
+message.innerHTML = row.Name + ": " + row.Message
+board.appendChild(message)
+console.log(row.Name)
+console.log(row.Message)
 }
